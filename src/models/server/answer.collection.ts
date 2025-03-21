b00da35ee6188bc94fdf93bc8fd1907077fb7dfc
Syed Ali Quadri <syedaliquadri100@gmail.com>
2025-03-22 00:25:48 +0530

    Add answer collection and its attributes.

---
+++ src/models/server/answer.collection.ts
@@ -0,0 +1,26 @@
+// Importing the stuffs from external/internal lib files.
+import { Permission } from "appwrite";
+
+// Importing the stuffs from local files.
+import { db, answerCollection } from "../name";
+import { databases } from "./config";
+
+export default async function createAnswerCollection() {
+    // Create question collection.
+    await databases.createCollection(db, answerCollection, answerCollection, [
+        Permission.read("any"),
+        Permission.read("users"),
+        Permission.create("users"),
+        Permission.update("users"),
+        Permission.delete("users"),
+    ]);
+    
+    console.log("Answer collection is created.");
+
+    // Assigning the attributes to the question collection.
+    await Promise.all([
+        databases.createStringAttribute(db, answerCollection, "content", 10000, true),
+        databases.createStringAttribute(db, answerCollection, "questionId", 50, true),
+        databases.createStringAttribute(db, answerCollection, "authorId", 50, true),
+    ]);
+}
