d4008395222bd845bad76b40d821e8e651101197
Syed Ali Quadri <syedaliquadri100@gmail.com>
2025-03-25 01:18:51 +0530

    Add answer deletion logic with reputation update

--- src/app/api/answer/route.ts
+++ src/app/api/answer/route.ts
@@ -7,21 +7,30 @@ export async function POST(request: NextRequest) {
 	try {
 		const { authorId, questionId, answer } = await request.json();
 
-        const response = await databases.createDocument(db, answerCollection, ID.unique(), {
-            content: answer,
-            authorId,
-            questionId
-        });
+		const response = await databases.createDocument(
+			db,
+			answerCollection,
+			ID.unique(),
+			{
+				content: answer,
+				authorId,
+				questionId
+			}
+		);
 
-        const prefs = await users.getPrefs(authorId);
-        await users.updatePrefs(authorId, {
-            reputation: Number(prefs.reputation) + 1
-        });
+		const prefs = await users.getPrefs(authorId);
+		await users.updatePrefs(authorId, {
+			reputation: Number(prefs.reputation) + 1
+		});
 
-        return NextResponse.json(response, {
-            status: 201
-        });
-
+		return NextResponse.json(
+			{
+				data: response
+			},
+			{
+				status: 201
+			}
+		);
 	} catch (error) {
 		NextResponse.json(
 			{
@@ -36,6 +45,32 @@ export async function POST(request: NextRequest) {
 
 export async function DELETE(request: NextRequest) {
 	try {
+		const { answerId } = await request.json();
+
+		const answer = await databases.getDocument(
+			db,
+			answerCollection,
+			answerId
+		);
+		const response = await databases.deleteDocument(
+			db,
+			answerCollection,
+			answerId
+		);
+
+		const prefs = await users.getPrefs(answer.authorId);
+		await users.updatePrefs(answer.authorId, {
+			reputation: Number(prefs.reputation) - 1
+		});
+
+		return NextResponse.json(
+			{
+				data: response
+			},
+			{
+				status: 201
+			}
+		);
 	} catch (error) {
 		NextResponse.json(
 			{
