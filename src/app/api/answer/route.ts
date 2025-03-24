2be9a7a7e008d57fb066afded71f71dc072d355a
Syed Ali Quadri <syedaliquadri100@gmail.com>
2025-03-25 01:14:42 +0530

    Add answer creation logic with user reputation update

--- src/app/api/answer/route.ts
+++ src/app/api/answer/route.ts
@@ -1,8 +1,27 @@
+import { answerCollection, db } from "@/models/name";
+import { databases, users } from "@/models/server/config";
 import { NextRequest, NextResponse } from "next/server";
+import { ID } from "node-appwrite";
 
 export async function POST(request: NextRequest) {
 	try {
 		const { authorId, questionId, answer } = await request.json();
+
+        const response = await databases.createDocument(db, answerCollection, ID.unique(), {
+            content: answer,
+            authorId,
+            questionId
+        });
+
+        const prefs = await users.getPrefs(authorId);
+        await users.updatePrefs(authorId, {
+            reputation: Number(prefs.reputation) + 1
+        });
+
+        return NextResponse.json(response, {
+            status: 201
+        });
+
 	} catch (error) {
 		NextResponse.json(
 			{
