2d20e64727bad1322b5c8e6425ef659df446c832
Syed Ali Quadri <syedaliquadri100@gmail.com>
2025-03-25 01:09:01 +0530

    Remove debug logging from answer creation API route

--- src/app/api/answer/route.ts
+++ src/app/api/answer/route.ts
@@ -3,9 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(request: NextRequest) {
 	try {
 		const { authorId, questionId, answer } = await request.json();
-		console.log(authorId);
-		console.log(questionId);
-		console.log(answer);
 	} catch (error) {
 		NextResponse.json(
 			{
