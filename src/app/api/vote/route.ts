4696093b6abcbc9ad0b73b3da8f5cc6ae5d91207
Syed Ali Quadri <syedaliquadri100@gmail.com>
2025-03-25 23:53:50 +0530

    Implement vote submission logic with document deletion and error handling

--- src/app/api/vote/route.ts
+++ src/app/api/vote/route.ts
@@ -1,14 +1,39 @@
+import { answerCollection, db, questionCollection, voteCollection } from "@/models/name";
+import { databases, users } from "@/models/server/config";
 import { NextRequest, NextResponse } from "next/server";
+import { Query } from "node-appwrite";
 
 export async function POST(request: NextRequest) {
 	try {
-        const { votedById, voteStatus, type, typeId } = await request.json();
+		const { votedById, voteStatus, type, typeId } = await request.json();
 
+		const response = await databases.listDocuments(db, voteCollection, [
+			Query.equal("type", type),
+			Query.equal("typeId", typeId),
+			Query.equal("votedById", votedById)
+		]);
+
+		console.log(response);
+
+		if (response.documents.length > 0) {
+			await databases.deleteDocument(
+				db,
+				voteCollection,
+				response.documents[0].$id
+			);
+		}
+
+        const answerOrQuestion = await databases.getDocument(db, type === "question" ? questionCollection : answerCollection, typeId);
+
+        // const authorPrefs = await users.getPrefs(answerOrQuestion.authodId);
 	} catch (error) {
-		return NextResponse.json({
-			error: error?.message || "Error while voting."
-		}, {
-            status: error.status || error.code || 500
-        });
+		return NextResponse.json(
+			{
+				error: error?.message || "Error while voting."
+			},
+			{
+				status: error.status || error.code || 500
+			}
+		);
 	}
 }
